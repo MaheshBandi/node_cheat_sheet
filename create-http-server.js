df26809939b957abe39f220a489be717b6389e02
Mahesh Bandi <mbandi1985@gmail.com>
2020-07-20 05:00:51 +0530

    Alternate way to create a server through events

--- create-http-server.js
+++ create-http-server.js
@@ -2,18 +2,46 @@
 
 const http = require('http');
 
-
+//1) Basic Example
 // http.createServer((req,res)=>{
 //     res.end('Hello from server')
 // }).listen("8000",()=>{
 //     console.log("Server started at port 8000");
 // });
 
-//Alternate way by assigning to a local variable
-const server = http.createServer((req,res)=>{
-    res.end('Hello from server')
+//####################################################################################
+//2) Alternate way by assigning to a local variable
+// const server = http.createServer((req,res)=>{
+//     res.end('Hello from server')
+// });
+
+// server.listen("8000",()=>{
+//          console.log("Server started at port 8000");
+// });
+
+//########################################################################################
+
+//3) Alternate way to create server through events
+
+const server = http.createServer();
+
+server.on('request',(req,res)=>{
+    console.log(req.url);
+    console.log('Request Received');
+    setTimeout(()=>{ res.end("Request Received");},10000);    
 });
 
-server.listen("8000",()=>{
+//using this approcah we can listen to multiple listners, but we can give only one response at a time
+server.on('request',(req,res)=>{
+    console.log(req.url);
+    console.log('Request Received _1');
+    //res.end("Request Received");
+});
+
+server.on("close",()=>{    
+    console.log('Indicates that the underlying connection was terminated.');
+})
+
+server.listen("8000","127.0.0.1",()=>{
          console.log("Server started at port 8000");
 });
