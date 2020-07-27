de386bc3d638255aa596037dbbe7d85593fdecc9
Mahesh Bandi <mbandi1985@gmail.com>
2020-07-27 16:03:57 +0530

    Seperated the Middlewares ,Routes , Controllers

--- express-example/app/app.js
+++ express-example/app/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const app = express();
 const morgan = require('morgan');
-
+const tourRouter = require('../routes/tourRoutes');
 //middleware to read the post boday
 app.use(express.json());
 app.use(morgan('dev')); //use morgan  to log the incomming request in the console or log
@@ -14,111 +12,6 @@ app.use((req,res,next)=>{
     next();
 });
 
-// app.get('/',(req,res)=>{
-//     //res.status(200).send('Hello from server side!');
-//     // or send json directly
-//     res.status(200).json({message:'Hello from the server side !'});
-
-// })
-
-const dirPath = path.join(__dirname, '../helpers/data/tours-simple.json');
-console.log(dirPath);
-const tours = JSON.parse(fs.readFileSync(dirPath));
-//console.log(tours.length);
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  //console.log(req.params);
-  const id = req.params.id * 1; // use this trick to convert string to int
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID',
-    });
-  }
-
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
-//By Default the data for post is not available in the req. we need to use a middleware  express.json()
-const createTour = (req, res) => {
-  //console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  //const newTour=  Object.assign({id:newId},req.body);
-  //or use Es6 Spread Operator
-  const newTour = { id: newId, ...req.body };
-  //tours.push(newTour);
-  const updatedTour = [...tours, newTour];
-  fs.writeFile(dirPath, JSON.stringify(updatedTour, null, 2), (err) => {
-    if (err) return console.log(err);
-    res.status(201).json(newTour);
-  });
-};
-
-const updateTour = (req, res) => {
-  const id = req.params.id * 1; // use this trick to convert string to int
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID',
-    });
-  }
-  const tour = tours.find((el) => el.id === id);
-  const updatedTour = { ...tour, ...req.body };
-  tours[id] = updatedTour;
-  fs.writeFile(dirPath, JSON.stringify(tours, null, 2), (err) => {
-    if (err) return console.log(err);
-    res.status(204).json(updatedTour);
-  });
-};
-
-const deleteTour = (req, res) => {
-  const id = req.params.id * 1; // use this trick to convert string to int
-  const tour = tours.find((el) => el.id === id);
-  if (!tour) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID',
-    });
-  }
-
-  const filterItems = tours.filter((el) => el.id != id);
-  console.log(filterItems);
-
-  fs.writeFile(dirPath, JSON.stringify(filterItems, null, 2), (err) => {
-    if (err) return console.log(err);
-    res.status(204).json({});
-  });
-};
-//app.get('/api/v1/tours', getAllTours);
-//app.get('/api/v1/tours/:id/:optionalParam?', getTour); //use ? to specify the optional params
-//app.post('/api/v1/tours', createTour);
-//app.patch('/api/v1/tours/:id', updateTour);
-//app.delete('/api/v1/tours/:id', deleteTour);
-
-//use app.route to combine the common routes
-//app.route('/api/v1/tours').get(getAllTours).post(createTour);
-//app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-//use router as a middleware and mounting a new router on a route
-const tourRouter = express.Router();
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 app.use('/api/v1/tours',tourRouter);
 
 const port = 3000;
---
+++ express-example/controllers/tourController.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = path.join(__dirname, '../helpers/data/tours-simple.json');
+const tours = JSON.parse(fs.readFileSync(dirPath));
+
+ exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1; // use this trick to convert string to int
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  }
+
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  const updatedTour = [...tours, newTour];
+  fs.writeFile(dirPath, JSON.stringify(updatedTour, null, 2), (err) => {
+    if (err) return console.log(err);
+    res.status(201).json(newTour);
+  });
+};
+
+exports.updateTour = (req, res) => {
+  const id = req.params.id * 1; // use this trick to convert string to int
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+  const updatedTour = { ...tour, ...req.body };
+  tours[id] = updatedTour;
+  fs.writeFile(dirPath, JSON.stringify(tours, null, 2), (err) => {
+    if (err) return console.log(err);
+    res.status(204).json(updatedTour);
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  const id = req.params.id * 1; // use this trick to convert string to int
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  }
+
+  const filterItems = tours.filter((el) => el.id != id);
+  
+  fs.writeFile(dirPath, JSON.stringify(filterItems, null, 2), (err) => {
+    if (err) return console.log(err);
+    res.status(204).json({});
+  });
+};
