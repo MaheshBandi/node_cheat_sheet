f098d1adfd6963f71bb3e5c719310ab8ba85855a
Mahesh Bandi <mbandi1985@gmail.com>
2020-07-28 17:31:19 +0530

    added params middleware

--- express-example/routes/tourRoutes.js
+++ express-example/routes/tourRoutes.js
@@ -2,6 +2,13 @@ const express =  require('express');
 const {getAllTours,createTour,getTour,updateTour,deleteTour} = require('../controllers/tourController');
 
 const router = express.Router();
+
+//use param middleware when we want to use soming when the request come for a specific parameneter option
+router.param('id',(req,res,next,value)=>{
+    console.log(`Hello from Param middleware value : ${value}`);
+    next();
+});
+
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 module.exports = router;
